5d4f7fd1a46b51a87d8b75f93f8ba97398c34962
AhmadShaarif <ahmadshaarif12@gmail.com>
2024-03-15 19:24:36 +0530

    Backend functionality added to dashboard, xml yet to be completed

--- app/src/main/java/com/example/myapplication/Dashboard_Activity.java
+++ app/src/main/java/com/example/myapplication/Dashboard_Activity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,38 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Dashboard_Activity extends AppCompatActivity {
+    private ImageButton settings;
+    private ImageButton cardetails;
+    private ImageButton prevrides;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
+        settings = findViewById(R.id.settings);
+        cardetails = findViewById(R.id.cardetails);
+        prevrides = findViewById(R.id.prevrides);
+
+        prevrides.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Dashboard_Activity.this, Prevrides_Activity.class));
+            }
+        });
+        cardetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Dashboard_Activity.this, Cardetails_Activity.class));
+            }
+        });
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Dashboard_Activity.this, Settings_Activity.class));
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
