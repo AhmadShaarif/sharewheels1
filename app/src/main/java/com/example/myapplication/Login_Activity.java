fefec327117a70c918326bb7f7883cd4eb9aef09
AhmadShaarif <ahmadshaarif12@gmail.com>
2024-03-15 07:25:24 +0530

    New changes made to login and registration

--- app/src/main/java/com/example/myapplication/Login_Activity.java
+++ app/src/main/java/com/example/myapplication/Login_Activity.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,52 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login_Activity extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
+    private Button loginbtn;
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginbtn = findViewById(R.id.loginbtn);
+
+        auth = FirebaseAuth.getInstance();
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+                loginuser(txt_email, txt_password);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    private void loginuser(String email, String password) {
+
+        auth.signInWithEmailAndPassword(email , password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(Login_Activity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Login_Activity.this , Dashboard_Activity.class));
+                finish();
+            }
         });
     }
 }
